b4fbab18bfa4a7171f16ff26aa44037320013058
Bogdan Vasile <vasileeduardbogdan@gmail.com>
2021-02-01 20:35:04 +0200

    test commit

---
+++ src/components/InboxScreen.vue
@@ -0,0 +1,14 @@
+<!-- src/components/InboxScreen.vue -->
+
+<template>
+	<PureInboxScreen :error="false" />
+</template>
+
+<script>
+import PureInboxScreen from './PureInboxScreen';
+
+export default {
+	name: 'InboxScreen',
+	components: { PureInboxScreen },
+};
+</script>
