9b4d7437d6dcd2516a3cf5cdf644026057633743
Bogdan Vasile <vasileeduardbogdan@gmail.com>
2021-02-02 00:18:05 +0200

    Update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
+// import Vue from "vue";
+// import App from "./App.vue";
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
+// new Vue({
+//   render: h => h(App)
+// }).$mount("#app");
+
+import DummyButton from './components/DummyButton.vue'
+export default DummyButton
